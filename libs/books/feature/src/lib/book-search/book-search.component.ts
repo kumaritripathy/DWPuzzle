88df93a31d6a9b694a6d48acea06850ed97f78e8
ankita tripathy <in_kumarit@m1.com.sg>
2023-10-03 15:18:49 +0530

    Task-2 Changes

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,17 +9,18 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.scss']
+  styleUrls: ['./book-search.component.scss'],
 })
 export class BookSearchComponent implements OnInit {
   books: ReadingListBook[];
 
   searchForm = this.fb.group({
-    term: ''
+    term: '',
   });
 
   constructor(
@@ -32,11 +33,18 @@ export class BookSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.store.select(getAllBooks).subscribe(books => {
+    this.store.select(getAllBooks).subscribe((books) => {
       this.books = books;
     });
+    this.onSearchBookChange()
   }
-
+  onSearchBookChange() {
+    this.searchForm.controls.term.valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((change) => {
+        return this.store.dispatch(searchBooks({ term: this.searchTerm }));
+      });
+  }
   formatDate(date: void | string) {
     return date
       ? new Intl.DateTimeFormat('en-US').format(new Date(date))
